f37b9f23b609d25366365a6e31e2472cdaa225b4
Atif moin <70331815+Atifmoin19@users.noreply.github.com>
2020-10-04 20:07:45 +0530

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,26 +1,13 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$message = $_REQUEST['message'];
 
 
-$email_form = '1900300100051@ipec.org.in';
-
-$email_subject = "New Form Submission";
-
-$email_body = "User Name: $name.\n".
-			   "User Email: $visitor_email.\n".
-			   "User Message: $message.\n".;
-
-			   $to = "atifmoin19@gmail.com";
-
-			   $header = "Form: $email_form \r\n";
-
-			   $header .= "Reply-To: $visitor_email \r\n";
-
-			   mail($to,$email_subject,$email_body);
-			   
-			   header("Location: index.html");
+mail("atifmoin19@gmail.com", "Feedback Profile",$message , "From: $name < $email");
+echo "<script type='text/javascript'>alert('Messgae successfully sent');
+window.history.log(-1);
+</script>";
 
 
 ?>
